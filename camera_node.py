9d9890760ed7351fc79e222481efeb8c3806ca42
cmcdermitt <charliemc1996@gmail.com>
2018-10-20 15:09:50 -0400

    updated camera_node

--- camera_node.py
+++ camera_node.py
@@ -7,9 +7,8 @@ from sensor_msgs.msg import Image
 cam= cv2.VideoCapture(0)
 bridge = cv_bridge.CvBridge()
 
-pub = rospy.Publisher('inception_input', Image, queue_size=10)
+pub = rospy.Publisher('imgs', Image, queue_size=10)
 rospy.init_node('camera', anonymous=True)
-rate = rospy.Rate(6) #runs at 10 hz to avoid flooding the NN
 
 mean = 128.0
 std = 1.0/128.0
@@ -19,4 +18,3 @@ while not rospy.is_shutdown():
 
     msg = bridge.cv2_to_imgmsg(cvimg)
     pub.publish(msg)
-rate.sleep()
